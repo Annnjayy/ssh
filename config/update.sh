22d439ae87814f376215b2fd5dc6ae8667e0172f
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-04-22 13:37:17 +0700

    Update update.sh

--- config/update.sh
+++ config/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget -O /tmp/menu-master.zip "https://raw.githubusercontent.com/Annnjayy/ssh/main/config/menu.zip" >/dev/null 2>&1 >> /etc/geostore/update.log
-mkdir /tmp/menu
+wget -O /tmp/menu-master.zip "https://raw.githubusercontent.com/Annnjayy/ssh/main/config/menu.zip" >> /etc/geostore/update.log
 7z e  /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
 chmod +x /tmp/menu/*
 mv /tmp/menu/* /usr/sbin/
